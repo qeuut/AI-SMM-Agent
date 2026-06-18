077e9e2473ae6b8da859adccea28298b5bc87ed0
qeuut <misa27830@gmail.com>
2026-06-18 22:23:33 +0300

    1

--- app/bot.py
+++ app/bot.py
@@ -8,7 +8,7 @@ from redis.asyncio import Redis
 from AI_SMM_AGENT.app.api.server import run_api_server # для railway
 from AI_SMM_AGENT.app.config.settings import settings
 from AI_SMM_AGENT.app.handlers import main_router as main_router
-from AI_SMM_AGENT.app.middlewares.admin_only import AdminOnlyMiddleware
+# from AI_SMM_AGENT.app.middlewares.admin_only import AdminOnlyMiddleware
 from AI_SMM_AGENT.app.utils.logger import logger
 from AI_SMM_AGENT.app.middlewares.answer_logger import CallbackAnswerLogger
 from AI_SMM_AGENT.app.services.database import connect_db, init_db, disconnect_db
@@ -36,7 +36,7 @@ async def main():
     dp.workflow_data["bot"] = bot
 
     # middlewares
-    dp.message.middleware(AdminOnlyMiddleware())
+    # dp.message.middleware(AdminOnlyMiddleware())
     # dp.callback_query.middleware(AdminOnlyMiddleware())
     dp.message.middleware(CallbackAnswerLogger())
     dp.callback_query.middleware(CallbackAnswerLogger())
