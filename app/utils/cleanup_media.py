277bd92a75f0f12e7c6412678892b33d07c24f23
qeuut <misa27830@gmail.com>
2026-06-24 21:19:17 +0300

    1

--- app/utils/cleanup_media.py
+++ app/utils/cleanup_media.py
@@ -7,16 +7,17 @@ async def cleanup_media_messages(bot, chat_id: int, state):
     data = await state.get_data()
     sent_messages = data.get("sent_message")
 
+
     logger.debug(
         f"sent_messages={sent_messages}, "
         f"type={type(sent_messages)}, "
-        f"len={len(sent_messages) if sent_messages else 0}"
-        f"item_type={type(sent_messages[0])}"
+        f"len={len(sent_messages) if sent_messages else 0},"
+        f"item_type={type(sent_messages[0]) if sent_messages and len(sent_messages) > 0 else 'None'}"
     )
 
     if sent_messages:
         sent_messages.pop(-1) # не удаляем текстовую часть потому что она всегда идет последней, а у нас везде edit_text
-        await bot.delete_messages(chat_id=chat_id, message_ids=sent_messages)
+        await bot.delete_messages(chat_id=chat_id, message_ids=sent_messages) # sent_messages
         logger.info(f"Сообщения {sent_messages} были удалены")
 
     else:
