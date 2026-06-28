e114a922eb4e061cf4a208fac70f0eecb50dafcf
qeuut <misa27830@gmail.com>
2026-06-28 21:48:13 +0300

    Поменял кнопки 'Уведомления' и 'О боте / Помощь' местами

--- app/keyboards/settings_inline.py
+++ app/keyboards/settings_inline.py
@@ -11,8 +11,8 @@ logger = logging.getLogger(__name__)
 
 def settings_main_btn():
     return create_buttons(
-        texts=["📢 Telegram-канал", "ℹ️ О боте / Помощь", "🔔 Уведомления", "⬅️ Вернуться в главное меню"], # Уведомления: Включены
-        callbacks=["select_channel_settings", "help_settings", "notifications", "MainMenu"],
+        texts=["📢 Telegram-канал", "🔔 Уведомления", "ℹ️ О боте / Помощь", "⬅️ Вернуться в главное меню"], # Уведомления: Включены
+        callbacks=["select_channel_settings", "notifications", "help_settings", "MainMenu"],
         net=[2,1,1]
     )
 
