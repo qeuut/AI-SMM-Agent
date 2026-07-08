81d91c1a241a7d2a5bd63306208cb99a45679c10
qeuut <misa27830@gmail.com>
2026-07-08 11:58:28 +0300

    refactor(content_plan): вынесен главный текст с функции content_plan

--- app/handlers/content_plan.py
+++ app/handlers/content_plan.py
@@ -13,6 +13,8 @@ from AI_SMM_AGENT.app.keyboards import (
 # модели
 from AI_SMM_AGENT.app.models.callbacks import CallbacksContentPlan
 
+# текста
+from AI_SMM_AGENT.texts.content_plan_texts import MAIN_TEXT
 
 content_plan_router = Router()
 
@@ -22,19 +24,4 @@ logger = logging.getLogger(__name__)
 
 @content_plan_router.callback_query(F.data == CallbacksContentPlan.CONTENT_PLAN)
 async def content_plan(callback: CallbackQuery) -> None:
-    text = (
-        "<b>💡 Контент-план</b>\n\n"
-
-        "Бот поможет составить пошаговую контент-стратегию для вашего канала на неделю или месяц вперед.\n\n"
-
-        "<b>Что умеет система:</b>\n"
-        "» Генерировать рубрики и конкретные темы для публикаций\n"
-        "» Распределять посты по дням недели для удержания аудитории\n"
-        "» Адаптировать идеи под тематику и Tone of Voice вашего бренда\n\n"
-
-        "<blockquote>Для точной настройки вы можете загрузить описание вашего канала или примеры старых постов в разделе «Стиль бренда».</blockquote>\n\n"
-
-        "Введите ключевую тему, нишу вашего канала или пришлите краткое описание идеи для генерации контент-плана... 🤖"
-    )
-
-    await callback.message.edit_text(text=text,reply_markup=back_to(), parse_mode="HTML")
+    await callback.message.edit_text(text=MAIN_TEXT,reply_markup=back_to(), parse_mode="HTML")
