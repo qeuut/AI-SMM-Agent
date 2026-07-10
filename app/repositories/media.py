f31bafc3d922077bc37e0e6975892b177ff9d893
qeuut <misa27830@gmail.com>
2026-07-10 19:01:20 +0300

    .

--- app/repositories/media.py
+++ app/repositories/media.py
@@ -11,19 +11,19 @@ async def change_media_ids(chat_id: int, selected_media_ids: list[str]) -> bool:
     media_ids_str = ",".join(selected_media_ids) # 123,456,789
 
     try:
-        await db.execute("UPDATE users SET selected_media_ids = ? WHERE chat_id = ?", (media_ids_str, chat_id))
+        await db.execute("UPDATE posts SET selected_media_ids = ? WHERE chat_id = ?", (media_ids_str, chat_id))
         await db.commit()
         logger.info("selected_media_ids успешно записано в БД")
 
     except aiosqlite.Error as e:
-        logger.critical(f"Ошибка при записи selected_media_ids в БД: {e}")
+        logger.critical(f"Ошибка при записи selected_media_ids({media_ids_str}) в БД: {e}")
         return False
     return True
 
 
 async def get_media_from_db(chat_id: int) -> list | None:
     db = await get_db()
-    cursor = await db.execute("SELECT selected_media_ids FROM users WHERE chat_id = ?", (chat_id,))
+    cursor = await db.execute("SELECT selected_media_ids FROM posts WHERE chat_id = ?", (chat_id,))
     row = await cursor.fetchone() or None
     await cursor.close()
 
