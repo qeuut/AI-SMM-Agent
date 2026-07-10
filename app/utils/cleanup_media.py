e90f8e27463fd43133f0827b12136c902c332028
qeuut <misa27830@gmail.com>
2026-07-10 14:21:43 +0300

    .

--- app/utils/cleanup_media.py
+++ app/utils/cleanup_media.py
@@ -5,30 +5,60 @@ logger = logging.getLogger(__name__)
 
 async def cleanup_media_messages(bot, chat_id: int, state):
     data = await state.get_data()
-    # sent_messages = data.get("sent_message")
-    selected_media = data.get("selected_media")
+    sent_messages = data.get("sent_message")
 
     logger.debug(
-        f"sent_messages={selected_media}, "
-        f"type={type(selected_media)}, "
-        f"len={len(selected_media) if selected_media else 0},"
-        f"item_type={type(selected_media[0]) if selected_media and len(selected_media) > 0 else 'None'}"
+        f"sent_messages={sent_messages}, "
+        f"type={type(sent_messages)}, "
+        f"len={len(sent_messages) if sent_messages else 0},"
+        f"item_type={type(sent_messages[0]) if sent_messages and len(sent_messages) > 0 else 'None'}"
     )
 
-    if selected_media and len(selected_media) > 0:
-        if len(selected_media) == 1:
+    if sent_messages and len(sent_messages) > 0:
+        if len(sent_messages) == 1:
             logger.info("В списке только 1 сообщение (текст), отмена удаления")
             return
 
-        selected_media.pop(-1)
+        sent_messages.pop(-1)
 
-        if len(selected_media) == 1:
-            await bot.delete_message(chat_id=chat_id, message_id=selected_media[0])
-            logger.info(f"Одиночное медиа-сообщение {selected_media[0]} было удалено")
+        if len(sent_messages) == 1:
+            await bot.delete_message(chat_id=chat_id, message_id=sent_messages[0])
+            logger.info(f"Одиночное медиа-сообщение {sent_messages[0]} было удалено")
 
-        elif len(selected_media) > 1:
-            await bot.delete_messages(chat_id=chat_id, message_ids=selected_media)
-            logger.info(f"Медиа-сообщения {selected_media} были удалены")
+        elif len(sent_messages) > 1:
+            await bot.delete_messages(chat_id=chat_id, message_ids=sent_messages)
+            logger.info(f"Медиа-сообщения {sent_messages} были удалены")
 
     else:
         logger.info("Функция ---cleanup_media_messages--- список с id сообщений пуст")
+
+
+
+# async def cleanup_media_messages(bot, chat_id: int, state):
+#     data = await state.get_data()
+#     sent_messages = data.get("sent_message")
+#
+#     logger.debug(
+#         f"sent_messages={sent_messages}, "
+#         f"type={type(sent_messages)}, "
+#         f"len={len(sent_messages) if sent_messages else 0},"
+#         f"item_type={type(sent_messages[0]) if sent_messages and len(sent_messages) > 0 else 'None'}"
+#     )
+#
+#     if sent_messages and len(sent_messages) > 0:
+#         if len(sent_messages) == 1:
+#             logger.info("В списке только 1 сообщение (текст), отмена удаления")
+#             return
+#
+#         sent_messages.pop(-1)
+#
+#         if len(sent_messages) == 1:
+#             await bot.delete_message(chat_id=chat_id, message_id=sent_messages[0])
+#             logger.info(f"Одиночное медиа-сообщение {sent_messages[0]} было удалено")
+#
+#         elif len(sent_messages) > 1:
+#             await bot.delete_messages(chat_id=chat_id, message_ids=sent_messages)
+#             logger.info(f"Медиа-сообщения {sent_messages} были удалены")
+#
+#     else:
+#         logger.info("Функция ---cleanup_media_messages--- список с id сообщений пуст")
